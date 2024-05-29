54e4e00e49fe3552d293c6032eefdf5a03a59e37
Aaro Koinsaari <aaro.koinsaari@proton.me>
2024-05-29 21:13:05 +0300

    Implement registration and login functionality

--- src/owasp_demo/app/views.py
+++ src/owasp_demo/app/views.py
@@ -1,4 +1,9 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+
+from .models import RegisterForm, Profile, LoginForm
 
 
 def index(request):
@@ -6,8 +11,32 @@ def index(request):
 
 
 def register(request):
-    pass
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = User.objects.create_user(username=username, password=password)
+            user.save()
+            Profile.objects.create(user=user, email=request.POST['email'])
+            return redirect('login')
+    else:
+        form = RegisterForm()
+    return render(request, 'app/register.html', {"form": form})
 
 
 def login(request):
-    pass
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request)
+                return redirect('profile')  # TODO
+            else:
+                return HttpResponse("Invalid login")
+    else:
+        form = LoginForm()
+    return render(request, 'app/login.html', {'form': form})
