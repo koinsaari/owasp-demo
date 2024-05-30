f0c139e73cb3d02ebc5f99a742e6eaa69e6341b0
Aaro Koinsaari <aaro.koinsaari@proton.me>
2024-05-30 16:33:52 +0300

    Add base layout

--- src/owasp_demo/app/urls.py
+++ src/owasp_demo/app/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('login/', views.login, name='login'),
     path('register/', views.register, name='register'),
+    path('login/', views.login, name='login'),
     path('profile/', views.profile, name='profile'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='index'), name='logout'),
 ]
